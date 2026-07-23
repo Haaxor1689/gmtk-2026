1cf1b795d20050e5eecee53ef1b14aa3c9412cd1
Rien <rien.strootman@gmail.com>
2026-07-24 00:33:41 +0200

    Initial Gameplay mechanics
    Added multiple baggage props
    Implementing a cool fuel counter
    Trying to rework try_move global function to feature fuel cost (in progress)

--- src/grid_node.gd
+++ src/grid_node.gd
@@ -1,8 +1,10 @@
-extends Node2D
 class_name GridNode
+extends Node2D
 
 var grid_pos: Vector2 = Vector2.ZERO
 @export var is_pushable: bool = false
+@export var is_breakable: bool = false
+@export var push_cost: float = 2.0
 
 func _ready() -> void:
 	Global.align_to_grid(self)
@@ -16,3 +18,7 @@ func update_z_index() -> void:
 	# Update z_index based on Y position for proper depth sorting
 	# Base offset ensures objects are above background/walls but can be below foreground
 	z_index = Global.GRID_NODE_BASE_Z_INDEX + int(global_position.y)
+
+func check_breakable() -> void:
+	print("Checking if ", name, " is breakable: ", is_breakable)
+		
